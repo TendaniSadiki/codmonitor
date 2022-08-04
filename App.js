f7e851a5d18d6172c9e609d1b7074359ac41a169
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-08-04 14:56:28 +0200

    {components and  src } added

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/components/Home/Home';
 
 // or alternatively
-import API from "call-of-duty-api";
 
-
-import { ModernWarfare, platforms } from "call-of-duty-api";
 
 
 
 
 export default function App() {
-API.login(ssoToken: string);
-  try {
-    let data = await ModernWarfare.fullData(gamertag: string, platform: platforms)
- } catch(Error) {
-     //Handle Exception
- }
+
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   
+    <StatusBar style="auto" />
+    <Home/>
     </View>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View >
+      <Text>Open up App.js to start working on your app!</Text>
+      
+    </View>
+  )
+}
