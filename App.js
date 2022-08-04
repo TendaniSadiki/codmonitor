5a24565e67865527f671b3a0988b540255556662
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-08-04 14:23:37 +0200

    commit

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// or alternatively
+import API from "call-of-duty-api";
+
+
+import { ModernWarfare, platforms } from "call-of-duty-api";
+
+
+
+
 export default function App() {
+API.login(ssoToken: string);
+  try {
+    let data = await ModernWarfare.fullData(gamertag: string, platform: platforms)
+ } catch(Error) {
+     //Handle Exception
+ }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
