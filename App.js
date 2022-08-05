1e04ed349b38579b8a0f20a1703b5ebf4dbf76a0
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-08-05 17:06:41 +0200

    {test}

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/components/Home/Home';
+import React,{useState,useEffect} from 'react';
+
 
 // or alternatively
 
@@ -9,13 +10,33 @@ import Home from './src/components/Home/Home';
 
 
 export default function App() {
-
+  const [data,setData] = useState([]);
+  const [loading,setLoading] = useState(true)
+  
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+   useEffect(()=>{
+    fetch("https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/iShot%2321899/profile/type/mp", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  },[])
 
   return (
     <View style={styles.container}>
    
     <StatusBar style="auto" />
-    <Home/>
+    {
+        loading ? <Text>Loading ...</Text>:
+        data.map((post)=>(
+          <View style={{flex:1,alignItems: 'center',justifyContent: 'center'}}>
+            <Text style={{fontSize:30, fontWeight: 'bold'}}>{post.title}</Text>
+            <Text style={{fontSize:15, color:'blue'}} >{post.body}</Text>
+          </View>
+        ))
+      }
     </View>
   );
 }
