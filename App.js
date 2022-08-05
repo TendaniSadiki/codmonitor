b2a31de48fc7c012ba497c73973a30c80c06f160
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-08-05 17:52:30 +0200

    {log in } added

--- App.js
+++ App.js
@@ -1,51 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React,{useState,useEffect} from 'react';
+import React, { Component } from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import * as Linking from 'expo-linking';
+import Constants from 'expo-constants';
 
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Login to COD"
+          onPress={this._handleOpenWithLinking}
+          style={styles.button}
+        />
 
-// or alternatively
-
-
-
-
-
-export default function App() {
-  const [data,setData] = useState([]);
-  const [loading,setLoading] = useState(true)
-  
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+      </View>
+    );
+  }
+  _handleOpenWithLinking = () => {
+    Linking.openURL('https://profile.callofduty.com/cod/login');
   };
-   useEffect(()=>{
-    fetch("https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/iShot%2321899/profile/type/mp", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  },[])
-
-  return (
-    <View style={styles.container}>
-   
-    <StatusBar style="auto" />
-    {
-        loading ? <Text>Loading ...</Text>:
-        data.map((post)=>(
-          <View style={{flex:1,alignItems: 'center',justifyContent: 'center'}}>
-            <Text style={{fontSize:30, fontWeight: 'bold'}}>{post.title}</Text>
-            <Text style={{fontSize:15, color:'blue'}} >{post.body}</Text>
-          </View>
-        ))
-      }
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  button: {
+    marginVertical: 10,
   },
 });
+
+
